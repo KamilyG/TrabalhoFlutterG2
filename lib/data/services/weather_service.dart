966420f46d2d2d5a7366847e2f4556914a9d83de
Kamilly Grolli <kamily.grolli@baspan.com.br>
2025-05-23 13:53:02 -0300

    page developd along with permissions adjusted

--- lib/data/services/weather_service.dart
+++ lib/data/services/weather_service.dart
@@ -8,7 +8,7 @@ import 'package:http/http.dart' as http;
 
 class WeatherService {
 
-  static const BASE_URL = 'open weather map org link';
+  static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
   final String apiKey;
 
   WeatherService(this.apiKey);
--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/data/models/weather_model.dart';
+import 'package:flutter_weather_app/data/services/weather_service.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +10,45 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+  // api key
+  final _weathersService = WeatherService('12be6370e19460a2a5afe25bda79da76');
+  Weather? _weather;
+
+  // busca weather
+  _fetchWeather() async {
+    String cityName = await _weathersService.getCurrentCity();
+
+    try {
+      final weather = await _weathersService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // animação do weather
+  @override
+  void initState() {
+    super.initState();
+
+    // busca weather ao iniciar
+    _fetchWeather();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(_weather?.cityName ?? "Carregando cidade..."),
+
+          Text('${_weather?.temperature.round()}°C')
+        ],
+      ),
+    );
   }
 }
