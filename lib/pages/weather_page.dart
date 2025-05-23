d1769a7db4174a9f5a2f4d1888a4d439325df50c
Kamilly Grolli <kamily.grolli@baspan.com.br>
2025-05-23 14:36:03 -0300

    weather animations included

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_weather_app/data/models/weather_model.dart';
 import 'package:flutter_weather_app/data/services/weather_service.dart';
+import 'package:lottie/lottie.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -29,6 +30,28 @@ class _WeatherPageState extends State<WeatherPage> {
   }
 
   // animação do weather
+  String getWeatherAnimation(String? mainCondition) {
+    if (mainCondition == null) return 'assets/Sunny.json'; // animacao default
+
+    switch (mainCondition.toLowerCase()) {
+      case 'clouds':
+      case 'mist':
+      case 'smoke':
+      case 'haze':
+      case 'dust':
+      case 'fog':
+        return 'assets/Cloud.json';
+      case 'rain':
+      case 'drizzle':
+      case 'shower rain':
+        return 'assets/Rain.json';
+      case 'thunderstorm':
+        return 'assets/Thunder.json';
+      default:
+        return 'assets/Sunny.json';
+    }
+  }
+
   @override
   void initState() {
     super.initState();
@@ -44,8 +67,13 @@ class _WeatherPageState extends State<WeatherPage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          // cidade
           Text(_weather?.cityName ?? "Carregando cidade..."),
 
+          // animacao
+          Lottie.asset(getWeatherAnimation(_weather?.mainCondition)),
+
+          // temperatura
           Text('${_weather?.temperature.round()}°C')
         ],
       ),
